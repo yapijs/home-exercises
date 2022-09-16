9402c0c8a53370b00f06b277aa10e10183ee562f
yapijs <daviszarins90@gmail.com>
2022-09-16 12:08:40 +0300

    Removed unnecessary comments

--- Java-excersises/src/main/java/io/codelex/loops/practice/NaturalNumbers.java
+++ Java-excersises/src/main/java/io/codelex/loops/practice/NaturalNumbers.java
@@ -5,7 +5,6 @@ public class NaturalNumbers {
     public static void main(String[] args) {
         System.out.println("The first 10 natural numbers are: ");
 
-
         for (int i = 0; i < 10; i++) {
           System.out.println(i);
         }
