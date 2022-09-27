17378c0042c224fbae039b00da0c6e92f6092e80
yapijs <daviszarins90@gmail.com>
2022-09-27 11:00:08 +0300

    few improvements

--- Java-excersises/src/main/java/io/codelex/loops/practice/Exercise5.java
+++ Java-excersises/src/main/java/io/codelex/loops/practice/Exercise5.java
@@ -6,6 +6,9 @@ public class Exercise5 {
     //Write a program that asks the user to enter two words. The program then prints out both words on one line.
     // The words will be separated by enough dots so that the total line length is 30
     public static void main(String[] args) {
+
+        final int MAX_LINE_LENGTH = 30;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the first word: ");
         String first = scanner.next();
@@ -13,7 +16,7 @@ public class Exercise5 {
         String second = scanner.next();
 
         System.out.print(first);
-        for (int i = 0; i < 30 - first.length() - second.length(); i++) {
+        for (int i = 0; i < MAX_LINE_LENGTH - first.length() - second.length(); i++) {
             System.out.print(".");
         }
         System.out.println(second);
