5622ed23dc6b8fa25d0ad8f7a9eee7d35d6fd703
yapijs <daviszarins90@gmail.com>
2022-10-01 12:29:11 +0300

    restructure of folders (maven)

--- Java-excersises/src/main/java/io/other/Main2.java
+++ Java-excersises/src/main/java/io/other/Main2.java
@@ -1,7 +1,9 @@
+package io.other;
+
 public class Main2 {
     public static void main(String[] args) {
         //PK[11] == (1101 - (1*PK[1] + 6*PK[2] + 3*PK[3] + 7*PK[4] + 9*PK[5] + 10*PK[6] + 5*PK[7] + 8*PK[8] + 4*PK[9] + 2*PK[10])) mod 11
-        String pk = "220990-12009";
+        String pk = "111111-11533";
         pk = pk.replace("-", "");
         String[] stringArray = pk.split("");//toCharArray();
         int[] constants = {1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
