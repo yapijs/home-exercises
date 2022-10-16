2554a6581a76267045edb31df606f5fa7128fc2b
yapijs <daviszarins90@gmail.com>
2022-10-16 13:48:05 +0300

    task 5

---
+++ Java-excersises/src/main/java/io/other/java_advanced_test/exercise5/Main.java
@@ -0,0 +1,34 @@
+package io.other.java_advanced_test.exercise5;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/*## Exercise #5
+#### (generics + functional programming)
+Implement the generic partOf method, which will return % of items satisfying the condition based on any type of list and the function indicated as parameter.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        List<String> names = Arrays.asList("John", "Peter", "Angelina", "Bravo");
+        double percent = partOf(names, (String name) -> name.length() > 4);
+        System.out.println(percent); //Should print out 0.75 because 75% of names are longer than 4 letters
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        double evenPercent = partOf(numbers, (Integer n) -> n % 2 == 1);
+        System.out.println(evenPercent); //Should print out 0.5 because 50% of numbers are even
+    }
+
+    public static <T> double partOf(List<T> list, Predicate<T> function) {
+        int numberSatisfiesCondition = 0;
+        for (T item: list) {
+            if (function.test(item)) {
+                numberSatisfiesCondition++;
+            }
+        }
+        return (double) numberSatisfiesCondition / list.size();
+    };
+}
