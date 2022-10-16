b2b95287e6a1539c9e50bdcdbf2e737ca867c40b
yapijs <daviszarins90@gmail.com>
2022-10-16 14:23:52 +0300

    task2 - fixed getting size of list

--- Java-excersises/src/main/java/io/other/java_advanced_test/exercise2/Basket.java
+++ Java-excersises/src/main/java/io/other/java_advanced_test/exercise2/Basket.java
@@ -4,27 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Basket<T> {
-    private int numberOfItems;
     private List<T> listOfItems = new ArrayList<>();
 
     public int getNumberOfItems() {
-        return numberOfItems;
+        return listOfItems.size();
     }
 
     public void addToBasket(T item) {
-        if (numberOfItems == 10) {
+        if (listOfItems.size() == 10) {
             throw new BasketFullException("Basket is full! You cannot add any more items!");
         }
         listOfItems.add(item);
-        numberOfItems = listOfItems.size();
     }
 
     public void removeFromBasket() {
-        if (numberOfItems == 0) {
+        if (listOfItems.size() == 0) {
             throw new BasketEmptyException("Basket is empty! Nothing to remove!");
         }
         listOfItems.remove(listOfItems.size()-1);
-        numberOfItems = listOfItems.size();
     }
 
 }
